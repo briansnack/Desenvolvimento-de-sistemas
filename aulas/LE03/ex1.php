4799b2cdea10447b2d68f568fb75a9180d91e3bb
Brian Snack <snackbrian10@gmail.com>
2024-04-08 15:09:24 -0300

    Ex01 LE03

--- aulas/LE03/ex1.php
+++ aulas/LE03/ex1.php
@@ -1,7 +1,12 @@
 <?php
 
-
-
-
+    for ($i = 1; $i <= 20; $i++) { 
+        echo $i . " ";
+    }
+    echo "<br>";
+    for ($i = 1; $i <= 20; $i++) { 
+        echo $i;
+        echo "<br>";
+    }
 
 ?>
